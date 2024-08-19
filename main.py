4499fbdb23877b78b2569efcbe04fc46cdf59721
MS <sofiapronina418@gmail.com>
2024-08-19 22:14:10 +0300

    доработала игру

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ pygame.init()
 w=2048
 h=1233
 screen=pygame.display.set_mode((w,h))
-pc1=pygame.image.load('pc1.png')
+pg1=pygame.image.load('pg1.png')
 
-pc1 = pygame.transform.scale(pc1,(w,h))
-background=pc1
-pc2=pygame.image.load('pc2.png')
+pg1 = pygame.transform.scale(pg1,(w,h))
+background=pg1
+pc2=pygame.image.load('bgf.png')
 pc2=pygame.transform.scale(pc2,(w,h))
-start=pygame.image.load('START.png') 
-start = pygame.transform.scale(start,(w,h))
+start=pygame.image.load('plstar.png') 
+start = pygame.transform.scale(start,(512,200))
 start_hitbox=start.get_rect()
 start_hitbox.center=(w/2,h/2)
+
+gg=pygame.image.load("thogh.png")
+gg_hitbox=gg.get_rect()
+gg_hitbox.center=(1024,615)
+def lore():
+    screen.blit(gg, gg_hitbox)
+
+loreactive=False
+loreshow=False
 show_start=True
 running = True
 while running:
@@ -21,10 +30,16 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if start_hitbox.collidepoint(event.pos):
+                #screen.fill("black")
                 background=pc2
+                loreactive=True
                 show_start=False
+            if loreactive==True:
+                loreshow=True
     screen.blit(background,(0,0))
     if show_start:
         screen.blit(start,start_hitbox)
+    if loreshow:
+        screen.blit(gg,gg_hitbox)
     pygame.display.update()
 pygame.quit()
